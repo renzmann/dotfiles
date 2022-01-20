1602b22b0cb8fad251247557f33a424cc080b056
Robb Enzmann <robbenzmann@gmail.com>
2022-01-19 19:46:01 -0500

    remove install fish script

--- install-aws-fish.sh
+++
@@ -1,6 +0,0 @@
-cd /etc/yum.repos.d/
-sudo wget https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
-sudo yum install fish
-cd $HOME
-sudo yum install util-linux-user
-chsh -sh ($which fish)
